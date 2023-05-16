054135d6f9bdc1355a36e2a360a9c573d1bf4f37
NikitaUk <109801114+NikitaUk@users.noreply.github.com>
2023-05-16 17:15:09 +0500

    Update modalsWindow.py

--- modalsWindow.py
+++ modalsWindow.py
@@ -1,8 +1,7 @@
 from PyQt6.QtWidgets import QWidget, QLabel, QVBoxLayout, QStackedLayout, QLineEdit, QPushButton
 from PyQt6.QtCore import Qt, QTimer
-from PyQt6.QtGui import QFont, QPixmap
-import functools
-from testWidgets import StartWidget, TestWidget1, TestWidget2, TestWidget3, EndWidget
+import random as rnd
+from testWindow import StartWidget, TestWidget1, TestWidget2, TestWidget3, EndWidget
 
 class ModalLogin(QWidget):
     def __init__(self):
@@ -44,66 +43,63 @@ class ModalLogin(QWidget):
         self.stackLayout.setCurrentIndex(1)
 
 class CaptchaLogin(QWidget):
-    isExit = False
     counter = 0
-    acess = True
-    def update_timer(self, timer): 
+    timer = 0
+    def update_timer(self): 
         self.attempt.setText(f"Ваша программа заблокирована на {self.total_seconds} секунд")
-        if self.total_seconds == timer:
+        if self.total_seconds == 0:
             self.attempt.setText("")
             self.timeq.stop()
-            self.acess = True
+            self.btn.setEnabled(True)
         self.total_seconds -= 1
 
-    def capthca_timer(self, timer):
-        self.total_seconds = timer
-        self.timeq = QTimer(self)
-        self.timeq.setInterval(1000)
-        self.timeq.timeout.connect(functools.partial(self.update_timer, 0))
+    def start_timer(self):
+        self.btn.setEnabled(False)
+        self.total_seconds = self.timer
         self.timeq.start()
 
-    def captcha_clicked(self, captcha):
-        if self.acess:
+    def capthca_timer(self):
+        self.timeq = QTimer()
+        self.timeq.setInterval(1000)
+        self.timeq.timeout.connect(self.update_timer)
+        self.start_timer()
+
+    def captcha_clicked(self):
+        if self.captcha_edit.text() == self.cap_text:
             self.timer = 0
-            if captcha == "2vyk":
-                self.isExit = True
-                self.close()
-            else:
-                self.counter += 1
-                if  self.counter == 2:
-                    self.timer = 3
-                elif  self.counter == 3:
-                    self.timer = 5
-                elif  self.counter > 3:
-                    self.timer = 10
-            if self.timer > 0:
-                self.acess = False
-                self.capthca_timer(self.timer)
-    def closeEvent(self, e):
-        if(not self.isExit):
-            e.ignore()
+            self.close()
+        else:
+            self.counter += 1
+            if  self.counter == 2:
+                self.timer = 3
+            elif  self.counter == 3:
+                self.timer = 5
+            elif  self.counter > 3:
+                self.timer = 10
+        if self.timer > 0:
+            self.capthca_timer()
 
     def __init__(self):
         super().__init__()
         #window settings
+        self.setWindowFlag(Qt.WindowType.FramelessWindowHint)
         self.setWindowTitle("Подтвердите, что вы не робот")
         self.setFixedSize(450, 250)
         self.setWindowModality(Qt.WindowModality.ApplicationModal)
         #widgets
-        self.captcha = QLabel()
+        self.cap_text = str(rnd.randint(1000, 9999))
+        self.captcha = QLabel(f"<center>{self.cap_text}</center>")
         self.error = QLabel("<center>Введите текст с изображения</center>")
         self.captcha_edit = QLineEdit()
         self.attempt = QLabel("")
         self.btn = QPushButton("Ввести")
         #widget settings
-        font = QFont("Open sans", pointSize=15)
-        font_error = QFont("Open sans", pointSize=10)
-        self.error.setFont(font)
-        self.captcha_edit.setFont(font)
-        self.attempt.setFont(font_error)
-        self.captcha.setPixmap(QPixmap("./images/captcha.png"))
+        self.captcha.setObjectName("LabelCaptcha")
+        self.error.setObjectName("LabelModalWindow")
+        self.captcha_edit.setObjectName("LabelModalWindow")
+        self.attempt.setObjectName("LabelErrorModalWindow")
         #handler events
-        self.btn.clicked.connect(lambda: self.captcha_clicked(self.captcha_edit.text()))
+        self.btn.clicked.connect(self.captcha_clicked)
         #layout
         vbox = QVBoxLayout()
         vbox.addWidget(self.captcha)
@@ -112,3 +108,5 @@ class CaptchaLogin(QWidget):
         vbox.addWidget(self.attempt)
         vbox.addWidget(self.btn)
         self.setLayout(vbox)
+        with open('style.css') as style:
+            self.setStyleSheet(style.read())
